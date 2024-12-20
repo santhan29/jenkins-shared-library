70e72584cb132411e375d57c786359146535e026
santhan29 <santhanpeddaguravagari@gmail.com>
2024-12-20 17:22:31 +0530

    jenkins-shared-library

--- vars/nodeJSEKSPipeline.groovy
+++ vars/nodeJSEKSPipeline.groovy
@@ -53,7 +53,7 @@ def call(Map configMap){
         // } 
             stage('Docker build') {
                 steps { 
-                    withAWS(region: 'us-east-1', credentials: 'aws-credentials') {
+                    withAWS(region: 'us-east-1', credentials:  "aws-creds-${environment}") { 
                         sh """
                             aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.us-east-1.amazonaws.com
 
